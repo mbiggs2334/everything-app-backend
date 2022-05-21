56b076822ae0b98b1b92c401a110f32549ee8576
Matthew <mbiggs2334@gmail.com>
2022-05-21 11:14:49 -0400

    cors changes

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.use((req, res, next) => {
     res.append('Access-Control-Allow-Origin', ['*']);
     res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
     res.append('Access-Control-Allow-Headers', 'Content-Type');
-    next();
+    return next();
 });
 
 const authRoutes = require('./routes/auth');
