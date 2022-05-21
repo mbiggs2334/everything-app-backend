8846bee6a783c6083e0c353b365bf0686912459c
Matthew <mbiggs2334@gmail.com>
2022-05-21 11:19:30 -0400

    cors changes

--- app.js
+++ app.js
@@ -9,12 +9,6 @@ app.use(express.json());
 
 app.use(cors());
 app.use(authenticateJWT);
-app.use((req, res, next) => {
-    res.append('Access-Control-Allow-Origin', ['*']);
-    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
-    res.append('Access-Control-Allow-Headers', 'Content-Type');
-    return next();
-});
 
 const authRoutes = require('./routes/auth');
 const userRoutes = require('./routes/users');
